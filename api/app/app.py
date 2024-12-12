ade551bc9ba9783cdd89c89f4a10ef8eb720cf92
efimgolikov <golikovefim65@gmail.com>
2024-12-12 16:49:52 +0300

    ADD: MongoDb and skeleton Docker Compose

--- api/app/app.py
+++ api/app/app.py
@@ -1,4 +1,10 @@
+from fastapi import FastAPI
+from app.router.data import data_router
+
 class Application:
-    @classmethod
-    def run(cls):
-        print("Starting application")
+    def __init__(self):
+        self.app = FastAPI()
+        self.setup_routes()
+
+    def setup_routes(self):
+        self.app.include_router(data_router, prefix="/api")
--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from app.app import Application as app
-
+import uvicorn
+from app.app import Application 
 
 def main():
-    app.run()
+    application = Application()
+    uvicorn.run(application.app, host="0.0.0.0", port=8000)
+
 
 if __name__ == '__main__':
     main()
