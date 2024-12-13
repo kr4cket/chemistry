66f1aff8f8701edd69104b1350ee771ae1a2921d
S1GHE <golikovefim65@gmail.com>
2024-12-13 14:41:05 +0300

    fix: db

--- api/app/router/data.py
+++ api/app/router/data.py
@@ -5,6 +5,6 @@ data_router = APIRouter()
 
 @data_router.get("/")
 async def get_db_link():
-    db_url = os.getenv("MONGO_URI", "Mongo")
+    db_url = os.getenv("SQLALCHEMY_DATABASE_URI", "SQLALCHEMY_DATABASE_URI")
     return {"db_url": db_url}
 
