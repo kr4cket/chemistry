d0e5b1b7d9173dcfa178e316324f9aeadf708bd7
S1GHE <golikovefim65@gmail.com>
2024-12-13 15:31:08 +0300

    add: .env

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
+import os
 import uvicorn
 from app.app import Application 
 
 def main():
     application = Application()
-    uvicorn.run(application.app, host="0.0.0.0", port=8000)
+    uvicorn.run(application.app, host="0.0.0.0", port=os.getenv("API_PORT", 8000))
 
 
 if __name__ == '__main__':
