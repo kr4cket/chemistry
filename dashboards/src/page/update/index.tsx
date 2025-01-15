3a1581850da746a9098d07bea225e6575f81bfe5
S1GHE <golikovefim65@gmail.com>
2025-01-15 06:39:33 +0300

    add: form and fix container

--- dashboards/src/page/update/index.tsx
+++ dashboards/src/page/update/index.tsx
@@ -1,5 +1,11 @@
+import { DragDropCSVForm } from "@/feature/dragdrop";
+
 const Update = () => {
-  return <div>update</div>;
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <DragDropCSVForm />
+    </div>
+  );
 };
 
 export default Update;
--- dashboards/src/shared/components/container/ui/container.tsx
+++ dashboards/src/shared/components/container/ui/container.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 export const Container: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <main className={"flex flex-col gap-2 px-4 overflow-scroll"}>
+    <main className={"flex flex-col gap-2 px-4 overflow-scroll w-full"}>
       {children}
     </main>
   );
